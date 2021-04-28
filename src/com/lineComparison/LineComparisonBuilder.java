0812711870f052429be2fd58f06c707c9b848f88
Pratap Pawar <pratappawar1997@gmail.com>
2021-04-28 20:55:28 +0530

    Refactor: Added refactor the code and checking Equality of two lines using OOP

--- src/com/lineComparison/LineComparisonBuilder.java
+++ src/com/lineComparison/LineComparisonBuilder.java
@@ -1,67 +1,71 @@
 package com.lineComparison;
-import java.math.*;
+
 import java.util.Scanner;
-  
+
 public class LineComparisonBuilder {
-  
-    public static void main(String[] args)
-    {
-  
-    	System.out.println("Welcome to line comparison computation problem!!");
-    	Scanner sc=new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to line comparison computation problem!!");
+
+		LineComputation lc = new LineComputation();
+		lc.lineComputationEquality();
+
+	}
+}
+
+class LineComputation {
+
+	public void lineComputationEquality() {
+		Scanner sc = new Scanner(System.in);
 		System.out.print("plese Enter line co-ordinates x1=");
-		int x1=sc.nextInt();
+		int x1 = sc.nextInt();
 		System.out.print("plese Enter line co-ordinates x2=");
-		int x2=sc.nextInt();
+		int x2 = sc.nextInt();
 		System.out.print("plese Enter line co-ordinates y1=");
-		int y1=sc.nextInt();
+		int y1 = sc.nextInt();
 		System.out.print("plese Enter line co-ordinates x2=");
-		int y2=sc.nextInt();
-		int length_of_line1=(int)(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
-		int line1_length=(int)Math.sqrt(length_of_line1);
-	
-		
-		System.out.println("Length of The line1 is="+line1_length);
-		
+		int y2 = sc.nextInt();
+		int length_of_line1 = (int) (((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+		int line1_length = (int) Math.sqrt(length_of_line1);
+
+		System.out.println("Length of The line1 is=" + line1_length);
+
 		System.out.print("plese Enter line co-ordinates x3=");
-		int x3=sc.nextInt();
+		int x3 = sc.nextInt();
 		System.out.print("plese Enter line co-ordinates x4=");
-		int x4=sc.nextInt();
+		int x4 = sc.nextInt();
 		System.out.print("plese Enter line co-ordinates y3=");
-		int y3=sc.nextInt();
+		int y3 = sc.nextInt();
 		System.out.print("plese Enter line co-ordinates y4=");
-		int y4=sc.nextInt();
-		int length_of_line2=(int)(((x4-x3)*(x4-x3))+((y4-y3)*(y4-y3)));
-		int line2_length=(int)Math.sqrt(length_of_line2);
+		int y4 = sc.nextInt();
+		int length_of_line2 = (int) (((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3)));
+		int line2_length = (int) Math.sqrt(length_of_line2);
 		sc.close();
-		System.out.println("Length of The line2 is="+line2_length);
-	
-		Integer L1=new Integer(line1_length);
-		Integer L2=new Integer(line2_length);
-		//Using Equal Method
-		
-		if(L1.equals(L2)) {
+		System.out.println("Length of The line2 is=" + line2_length);
+
+		Integer L1 = new Integer(line1_length);
+		Integer L2 = new Integer(line2_length);
+		// Using Equal Method
+
+		if (L1.equals(L2)) {
 			System.out.println("Both lines are equal");
-		}
-		else {
+		} else {
 			System.out.println("Both lines are not equal");
 		}
-		
-		//Using CompareTo method
-		
-		int eq=L1.compareTo(L2);
+
+		// Using CompareTo method
+
+		int eq = L1.compareTo(L2);
 		System.out.println(eq);
-		if(eq==0) {
+		if (eq == 0) {
 			System.out.println("Bothe lines are Equal ");
-		}
-		else if(eq>0) {
+		} else if (eq > 0) {
 			System.out.println("Line1 is Greater Than Line 2");
-		}
-		else {
+		} else {
 			System.out.println("Line2 is Greater than Line1");
 		}
-		
-		
-        
-    }
+
+	}
+
 }
